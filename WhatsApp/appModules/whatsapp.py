6e878ace8d57dd71d002e61a08d1e9c15e1d0518
James Scholes <james@jamesscholes.com>
2025-04-19 13:24:17 -0600

    Remove ' list' suffix from 'Chats list' name

---
+++ WhatsApp/appModules/whatsapp.py
@@ -0,0 +1,47 @@
+from enum import Enum
+
+import addonHandler
+import appModuleHandler
+from controlTypes import Role
+from logHandler import log
+
+
+addonHandler.initTranslation()
+
+
+class WAAutomationID(Enum):
+	CHAT_LIST = 'ChatList'
+
+	def __str__(self):
+		return self.value
+
+
+CHAT_LIST_SUFFIX = ' list'
+
+def trimChatListName(obj):
+	obj.name = obj.name.removesuffix(CHAT_LIST_SUFFIX)
+
+
+_automationIDTransforms = {
+	WAAutomationID.CHAT_LIST: frozenset({trimChatListName}),
+}
+
+
+class AppModule(appModuleHandler.AppModule):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		log.info('WhatsApp::WhatsApp add-on initialised.')
+
+	def event_NVDAObject_init(self, obj):
+		try:
+			automationID = WAAutomationID(obj.UIAAutomationId)
+		except ValueError:
+			automationID = None
+
+		if automationID:
+			for transform in _automationIDTransforms.get(automationID, set()):
+				try:
+					transform(obj)
+				except Exception:
+					log.exception('WhatsApp::Error while applying transform %r for obj with automation ID "%s".', transform, automationID)
+					continue
